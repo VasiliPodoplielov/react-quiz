3ebf992d5c4027931cb4d9d15b9f914cf1c75a2d
Vasili Podoplielov <vasili.podoplelov@gmail.com>
2020-05-27 22:51:21 +0300

    Finnal app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import Layout from "./hoc/Layout/Layout";
+import {Redirect, Route, Switch, withRouter} from 'react-router-dom'
+import Quiz from "./containers/Quiz/Quiz";
+import QuizList from './containers/QuizList/QuizList';
+import Auth from './containers/Auth/Auth';
+import QuizCreator from './containers/QuizCreator/QuizCreator';
+import {connect} from 'react-redux';
+import Logout from './components/Logout/Logout';
+import {autoLogin} from './store/actions/auth';
 
-function App() {
+
+function App(props) {
+
+  useEffect(() => {
+    props.autoLogin();
+  })
+
+  let routes = (
+    <Switch>
+      <Route
+        path='/auth'
+        component={Auth}
+      />
+      <Route
+        path='/quiz/:id'
+        component={Quiz}
+      />
+      <Route
+        path='/'
+        exact
+        component={QuizList}
+      />
+    </Switch>
+  );
+
+  if (props.isAuthenticated) {
+    routes = (
+      <Switch>
+        <Route
+          path='/quiz-creator'
+          component={QuizCreator}
+        />
+        <Route
+          path='/quiz/:id'
+          component={Quiz}
+        />
+        <Route
+          path={'/logout'}
+          component={Logout}
+        />
+        <Route
+          path='/'
+          exact
+          component={QuizList}
+        />
+        <Redirect to={'/'} />
+      </Switch>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      {routes}
+    </Layout>
   );
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    isAuthenticated: !!state.auth.token
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    autoLogin: () => dispatch(autoLogin())
+  }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
--- src/index.js
+++ src/index.js
@@ -2,16 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import {applyMiddleware, createStore, compose} from 'redux';
+import {Provider} from 'react-redux';
+import rootReducer from './store/reducers/rootReducer';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    }) : compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(
+    thunk
+  ))
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const app = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+)
+
+ReactDOM.render(
+  app,
+  document.getElementById('root')
+);
 serviceWorker.unregister();
